1c8b3a8542f8a9f7fb38b32f9db5c016db712f19
Andrey Kozlov <andrey.kozlov@skuvault.com>
2022-04-20 16:19:45 +0500

    VT-5543: Add tests for FileSet supporting old Hourly format
    Old is "yyyyMMdd-HH", new is "yyyyMMddHH". We need to ensure that new format alone is supported, and that both are supported simultaneously.

--- test/Serilog.Sinks.Elasticsearch.Tests/FileSetTests.cs
+++ test/Serilog.Sinks.Elasticsearch.Tests/FileSetTests.cs
@@ -22,10 +22,7 @@ public class FileSetTests : IDisposable
 
     public void Dispose()
     {
-        foreach (var bufferFileName in _bufferFileNames.Values)
-        {
-            System.IO.File.Delete(bufferFileName);
-        }
+        foreach (var bufferFileName in _bufferFileNames.Values) System.IO.File.Delete(bufferFileName);
     }
 
     [Theory]
@@ -51,8 +48,53 @@ public class FileSetTests : IDisposable
         bufferFiles.Should().BeEquivalentTo(bufferFileForInterval);
     }
 
+    [Fact]
+    // Ensures that date format "yyyyMMdd-HH" is supported by Hourly interval. This date format was for Hourly files before move to standard "yyyyMMddHH"
+    public void GetBufferFiles_SupportOldHourlyFormat()
+    {
+        // Arrange
+        var rollingInterval = RollingInterval.Hour;
+        var format = "yyyyMMdd-HH";
+        _bufferFileNames = GenerateFilesUsingFormat(format);
+        var fileSet = new FileSet(_fileNameBase, rollingInterval);
+        var bufferFileForInterval = _bufferFileNames[rollingInterval];
+
+        // Act
+        var bufferFiles = fileSet.GetBufferFiles();
+
+        // Assert
+        bufferFiles.Should().BeEquivalentTo(bufferFileForInterval);
+    }
+
+
+    [Fact]
+    // Ensures that both date formats are supported simultaneously: "yyyyMMdd-HH"(old) and "yyyyMMddHH" (new)
+    public void GetBufferFiles_BothNewAndOldHourlyFormatsAreSupported_andOldSortedFirst()
+    {
+        // Arrange
+        var rollingInterval = RollingInterval.Hour;
+        var oldFormat = "yyyyMMdd-HH";
+        var newFormat = rollingInterval.GetFormat();
+        _bufferFileNames = new Dictionary<RollingInterval, string>
+        {
+            {RollingInterval.Hour, GenerateBufferFile(oldFormat, oldFormat)},
+            // adding with arbitrary interval to Dictionary for a proper clean up (already have Hour filled)
+            {RollingInterval.Day, GenerateBufferFile(oldFormat, newFormat)}
+        };
+        var fileSet = new FileSet(_fileNameBase, rollingInterval);
+        var hourlyBufferFileForOldFormat = _bufferFileNames[RollingInterval.Hour];
+        var hourlyBufferFileForNewFormat = _bufferFileNames[RollingInterval.Day];
+
+        // Act
+        var bufferFiles = fileSet.GetBufferFiles();
+
+        // Assert
+        bufferFiles.ShouldBeEquivalentTo(new[] {hourlyBufferFileForOldFormat, hourlyBufferFileForNewFormat},
+            options => options.WithStrictOrdering());
+    }
+
     /// <summary>
-    /// Generates buffer files for all RollingIntervals and returns dictionary of {rollingInterval, fileName} pairs.
+    ///     Generates buffer files for all RollingIntervals and returns dictionary of {rollingInterval, fileName} pairs.
     /// </summary>
     /// <param name="format"></param>
     /// <returns></returns>
@@ -61,14 +103,20 @@ public class FileSetTests : IDisposable
         var result = new Dictionary<RollingInterval, string>();
         foreach (var rollingInterval in Enum.GetValues(typeof(RollingInterval)))
         {
-            var bufferFileName = string.Format(_tempFileFullPathTemplate,
-                string.IsNullOrEmpty(format) ? string.Empty : new DateTime(2000, 1, 1).ToString(format));
-            var lines = new[] {rollingInterval.ToString()};
-            // Important to use UTF8 with BOM if we are starting from 0 position 
-            System.IO.File.WriteAllLines(bufferFileName, lines, new UTF8Encoding(true));
+            var bufferFileName = GenerateBufferFile(format, rollingInterval.ToString());
             result.Add((RollingInterval) rollingInterval, bufferFileName);
         }
 
         return result;
+    }
+
+    private string GenerateBufferFile(string format, string content)
+    {
+        var bufferFileName = string.Format(_tempFileFullPathTemplate,
+            string.IsNullOrEmpty(format) ? string.Empty : new DateTime(2000, 1, 1).ToString(format));
+        var lines = new[] {content};
+        // Important to use UTF8 with BOM if we are starting from 0 position 
+        System.IO.File.WriteAllLines(bufferFileName, lines, new UTF8Encoding(true));
+        return bufferFileName;
     }
 }
